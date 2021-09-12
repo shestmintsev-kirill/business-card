4a33d5fc90593082f824c7a013609d911cdf0f41
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-09-12 19:01:43 +0300

    reviews storage

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -24,20 +24,25 @@
               :src="review.sex ? getImage(`man.png`) : getImage('woman.png')"
               alt="client"
             />
-            <p class="section-reviews-name">{{ review.name }}</p>
+            <p class="section-reviews-name">
+              {{ $t(`Review.review_${index + 1}.name`) }}
+            </p>
           </div>
           <div class="section-reviews-description">
-            <h3>{{ review.title }}</h3>
+            <h3>{{ $t(`Review.review_${index + 1}.title`) }}</h3>
             <span>
               {{
                 review.showDescription
-                  ? $t(review.description)
-                  : $t(review.description).split(" ").slice(0, 11).join(" ")
+                  ? $t(`Review.review_${index + 1}.review`)
+                  : $t(`Review.review_${index + 1}.review`)
+                      .split(" ")
+                      .slice(0, 11)
+                      .join(" ")
               }}
             </span>
             <br v-if="review.showDescription" />
             <a
-              v-if="reviewLength(review.description) > 90"
+              v-if="reviewLength(`Review.review_${index + 1}.review`) > 90"
               @click.prevent="review.showDescription = !review.showDescription"
               href="/"
             >
@@ -73,146 +78,98 @@ export default {
     reviews: [
       {
         sex: false,
-        name: "Ольга",
-        title: "Консультация по инвестициям",
         platform: "telegram2.png",
         link: "https://t.me/olya_ermenina",
         showDescription: false,
-        description: "Review.review_1.review",
       },
       {
         sex: true,
-        name: "Михаил",
-        title: "Аналитика под ключ в Power BI",
         platform: "fl.png",
         link: "https://www.fl.ru/users/ashestmintseva/opinions/",
         showDescription: false,
-        description: "Review.review_2.review",
       },
       {
         sex: true,
-        name: "Эльдар",
-        title: "Обучение Power BI",
         platform: "profi.jpg",
         link: "https://profi.ru/profile/VarakinaAV3/",
         showDescription: false,
-        description: "Review.review_3.review",
       },
       {
         sex: true,
-        name: "Евгений",
-        title: "Консультация по Power Query",
         platform: "profi.jpg",
         link: "https://profi.ru/profile/VarakinaAV3/",
         showDescription: false,
-        description: "Review.review_4.review",
       },
       {
         sex: true,
-        name: "Денис",
-        title: "Консультация по Power BI",
         platform: "profi.jpg",
         link: "https://profi.ru/profile/VarakinaAV3/",
         showDescription: false,
-        description: "Review.review_5.review",
       },
       {
         sex: true,
-        name: "Сергей",
-        title: "Консультация по инвестициям",
         platform: "telegram2.png",
         link: "https://t.me/koraboom",
         showDescription: false,
-        description: "Review.review_6.review",
       },
       {
         sex: true,
-        name: "Тимофей",
-        title: "Обучение Power BI",
         platform: "profi.jpg",
         link: "https://profi.ru/profile/VarakinaAV3/",
         showDescription: false,
-        description: "Review.review_7.review",
       },
       {
         sex: false,
-        name: "Оксана",
-        title: "Консультация по Excel",
         platform: "profi.jpg",
         showDescription: false,
         link: "https://profi.ru/profile/VarakinaAV3/",
-        description: "Review.review_8.review",
       },
       {
         sex: false,
-        name: "Раиса",
-        title: "Обучение Excel",
         platform: "profi.jpg",
         link: "https://profi.ru/profile/VarakinaAV3/",
         showDescription: false,
-        description: "Review.review_9.review",
       },
       {
         sex: false,
-        name: "Марина",
-        title: "Аналитика под ключ в Power BI",
         platform: "fl.png",
         showDescription: false,
         link: "https://www.fl.ru/users/ashestmintseva/opinions/",
-        description: "Review.review_10.review",
       },
       {
         sex: false,
-        name: "Екатерина",
-        title: "Консультация по Power BI",
         platform: "profi.jpg",
         showDescription: false,
         link: "https://profi.ru/profile/VarakinaAV3/",
-        description: "Review.review_11.review",
       },
       {
         sex: true,
-        name: "Вадим",
-        title: "Консультация по Excel",
         platform: "profi.jpg",
         showDescription: false,
         link: "https://profi.ru/profile/VarakinaAV3/",
-        description: "Review.review_12.review",
       },
       {
         sex: false,
-        name: "Ирина",
-        title: "Консультация по Power BI",
         platform: "profi.jpg",
         showDescription: false,
         link: "https://profi.ru/profile/VarakinaAV3/",
-        description: "Review.review_13.review",
       },
       {
         sex: false,
-        name: "Анна",
-        title: "Обучение Excel",
         showDescription: false,
         link: "https://t.me/olya_ermenina",
-        description: "Review.review_14.review",
       },
       {
         sex: false,
-        name: "Ульяна",
-        title: "Сопровождение, Инвестиции",
         platform: "telegram2.png",
         showDescription: false,
         link: "https://t.me/kochetkova_ulya",
-        description: "Review.review_15.review",
       },
       {
         sex: false,
-        name: "Елена",
-        title: "Консультация по Power Query",
         platform: "profi.jpg",
         showDescription: false,
         link: "https://profi.ru/profile/VarakinaAV3/",
-        description: "Review.review_16.review",
       },
     ],
   }),
